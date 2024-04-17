bd444a35d460e570dd27b52a7bac58e5e3ad3b0f
Adrian Aquino-Neary <adrianaquino.aquino33@gmail.com>
2024-04-17 17:40:41 +0100

    Add matrix maths

--- src/managers/processManager.js
+++ src/managers/processManager.js
@@ -1,17 +1,4 @@
 const ProcessManager = (function () {
-  let state = 'nothing';
-
-  const maxFPS = 60;
-
-  const debugStates = true;
-
-
-  const checkerCol1 = new sRGB(28 / 255, 28 / 255, 28 / 255);
-  let temp = checkerCol1.copy();
-  temp.scalar(1 / 2);
-
-  const checkerCol2 = temp.copy();
-
   function drawChecker(size) {
     noStroke();
     for (let i = 0; i < Math.ceil(width / size); i++) {
@@ -33,6 +20,51 @@ const ProcessManager = (function () {
       }
     }
   }
+
+  let state = 'nothing';
+
+  const maxFPS = 60;
+
+  const debugStates = true;
+
+  const checkerCol1 = new sRGB(28 / 255, 28 / 255, 28 / 255);
+  let temp = checkerCol1.copy();
+  temp.scalar(1 / 2);
+
+  const checkerCol2 = temp.copy();
+
+  // testing
+
+  let temp1 = new Matrix([
+    [1, 0, 2],
+    [2, 1, 3],
+    [1, 0, 4]
+  ]);
+  let rhs1 = new Matrix([
+    [2, 6, 1],
+    [5, 7, 8]
+  ]);
+  let result1 = temp1.copy();
+  result1.mult(rhs1);
+
+  console.log(temp1);
+  console.log('times');
+  console.log(rhs1);
+  console.log('equals');
+  console.log(result1);
+  console.log('-----');
+
+  let temp2 = temp1.copy();
+  temp2.invert3x3();
+  let result2 = temp2.copy();
+  result2.mult(result1);
+
+  console.log(temp2);
+  console.log('times');
+  console.log(result1);
+  console.log('equals');
+  console.log(result2);
+  console.log('-----');
 
   return {
     changeState(s) {
